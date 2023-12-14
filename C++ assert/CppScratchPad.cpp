dc4b3fa058a5ba9ee91cf6f4d4354a1ead3ffca4
Kulpot <kulpotwahu@gmail.com>
2023-12-14 21:33:13 +0800

    ...

--- C++ assert/CppScratchPad.cpp
+++ C++ assert/CppScratchPad.cpp
@@ -2,7 +2,19 @@
 // ref link:https://www.youtube.com/watch?v=72dt8Fvr7x8&list=PLRwVmtr-pp056ahRbFrYbeAnIu2qvXPFe&index=5
 
 // assert - boolean expresiong - true(nochange) - false(someError)
-//#define assert(expression) (void)(                                                       \
+// #ifdef NDEBUG
+// 
+// #define assert(expression) ((void)0)		<-- compile out
+// 
+// #else
+// 
+// _ACRTIMP void __cdecl _wassert(
+// 	_In_z_ wchar_t const* _Message,
+// 	_In_z_ wchar_t const* _File,
+// 	_In_   unsigned       _Line
+// );
+// 
+// #define assert(expression) (void)(                                                       \
 	//            (!!(expression)) ||                                                              \
 	//            (_wassert(_CRT_WIDE(#expression), _CRT_WIDE(__FILE__), (unsigned)(__LINE__)), 0) \
 
@@ -16,7 +28,7 @@ int main()
 	int i = 5;
 	int j = i * i;
 	assert(j == 25);  // assert true
-	assert(j == 24);  // assert false , output:assertion failed: j == 24
+	//assert(j == 24);  // assert false , output:assertion failed: j == 24
 	cout << "continuing" << endl;
 
 	
