4f6f4308aa58c147129ac1d6027dedc6099621eb
Kulpot <kulpotwahu@gmail.com>
2023-12-14 21:21:08 +0800

    ...

--- C++ assert/CppScratchPad.cpp
+++ C++ assert/CppScratchPad.cpp
@@ -1,6 +1,8 @@
 // ---------------------------- C++ assert -----------------------------------------------------------
 // ref link:https://www.youtube.com/watch?v=72dt8Fvr7x8&list=PLRwVmtr-pp056ahRbFrYbeAnIu2qvXPFe&index=5
 
+// assert - boolean - true(nochange) - false(someError)
+
 #include <iostream>
 using std::cout;
 using std::endl;
