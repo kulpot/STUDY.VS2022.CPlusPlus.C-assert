f41d17de7fe9c260b962b01fb61f0f1298c21a4e
Kulpot <kulpotwahu@gmail.com>
2023-12-14 21:18:42 +0800

    Add project files.

---
+++ C++ assert/CppScratchPad.cpp
@@ -0,0 +1,11 @@
+// ---------------------------- C++ assert -----------------------------------------------------------
+// ref link:https://www.youtube.com/watch?v=72dt8Fvr7x8&list=PLRwVmtr-pp056ahRbFrYbeAnIu2qvXPFe&index=5
+
+#include <iostream>
+using std::cout;
+using std::endl;
+
+int main()
+{
+
+}
