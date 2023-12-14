bf0bf55fdeae63bbd79df6668490f73a10b3a7ba
Kulpot <kulpotwahu@gmail.com>
2023-12-14 21:29:25 +0800

    ..

--- C++ assert/CppScratchPad.cpp
+++ C++ assert/CppScratchPad.cpp
@@ -1,7 +1,10 @@
 // ---------------------------- C++ assert -----------------------------------------------------------
 // ref link:https://www.youtube.com/watch?v=72dt8Fvr7x8&list=PLRwVmtr-pp056ahRbFrYbeAnIu2qvXPFe&index=5
 
-// assert - boolean - true(nochange) - false(someError)
+// assert - boolean expresiong - true(nochange) - false(someError)
+//#define assert(expression) (void)(                                                       \
+	//            (!!(expression)) ||                                                              \
+	//            (_wassert(_CRT_WIDE(#expression), _CRT_WIDE(__FILE__), (unsigned)(__LINE__)), 0) \
 
 #include <cassert>
 #include <iostream>
@@ -15,5 +18,7 @@ int main()
 	assert(j == 25);  // assert true
 	assert(j == 24);  // assert false , output:assertion failed: j == 24
 	cout << "continuing" << endl;
+
+	
 
 }
